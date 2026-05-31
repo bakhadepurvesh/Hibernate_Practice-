606edba64873f0808f5b40bf8c0dc7f4517124f4
bakhade purvesh <purveshbakhade78@gmail.com>
2026-05-31 16:17:54 +0530

    Hibernate practice from nagpure

--- src/main/java/com/soft_repository/Crud_Logic.java
+++ src/main/java/com/soft_repository/Crud_Logic.java
@@ -1,5 +1,7 @@
 package com.soft_repository;
 
+import java.util.ArrayList;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -14,22 +16,45 @@ public class Crud_Logic {
 		
 		// insert the single data
 		
-//		 Students stu = new Students();
-//		stu.setName("ram");
-//		stu.setEmail("ram@gmail.com");
-//		stu.setCourse("java");
-//		stu.setMarks(80);
+		Students stu = new Students();
+		stu.setName("ramesh");
+		stu.setEmail("ramesh@gmail.com");
+		stu.setCourse("html");
+		stu.setMarks(85);
+		
+		Students stu1 = new Students();
+		stu1.setName("sita");
+		stu1.setEmail("sita@gmail.com");
+		stu1.setCourse("c++");
+		stu1.setMarks(70);
+		
+		Students stu2 = new Students();
+		stu2.setName("sita");
+		stu2.setEmail("sita@gmail.com");
+		stu2.setCourse("c++");
+		stu2.setMarks(70);
+		
+		
+		ArrayList<Students>list = new ArrayList();
+		
+		list.add(stu);
+		list.add(stu1);
+		list.add(stu2);
 		
 		
 	    SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
 	    
-	    
 	    Session session = sessionFactory.openSession();
-	    
 	    
 	    Transaction tran = session.beginTransaction();
 	    
+//		insert the single data
 //	    session.persist(stu);
+	    
+	    // multiple data insert 
+	    for(Students std : list){
+			session.persist(std);
+		}
 	    
 	    tran.commit();
 	    
